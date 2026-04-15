2d9928a91eb783f1d5587ebe75eaac1e690c7227
dicatop0001-maker <dicatop0001@gmail.com>
2026-04-14 22:22:49 -0300

    Feat: adicionar PrivateRoute + proteção de rotas + redirect 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-﻿import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { supabase } from './supabaseClient'
 import Login from './Login'
 import Home from './Home'
 import NovoLeilao from './NovoLeilao'
@@ -6,19 +8,33 @@ import DetalhesLeilao from './DetalhesLeilao'
 import MeusLeiloes from './MeusLeiloes'
 import VendedorPerfil from './VendedorPerfil'
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/novo" element={<NovoLeilao />} />
-        <Route path="/leilao/:id" element={<DetalhesLeilao />} />
-        <Route path="/meus-leiloes" element={<MeusLeiloes />} />
-        <Route path="/vendedor/:sellerId" element={<VendedorPerfil />} />
-      </Routes>
-    </Router>
-  )
+function PrivateRoute({ children }) {
+    const [session, setSession] = useState(undefined)
+
+  useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+                setSession(session)
+        })
+  }, [])
+
+  if (session === undefined) return null
+    return session ? children : <Navigate to="/" replace />
 }
 
-export default App
+function App() {
+    return (
+          <Router>
+                <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>PrivateRoute>} />
+                                <Route path="/novo" element={<PrivateRoute><NovoLeilao /></PrivateRoute>PrivateRoute>} />
+                                        <Route path="/leilao/:id" element={<PrivateRoute><DetalhesLeilao /></PrivateRoute>PrivateRoute>} />
+                                                <Route path="/meus-leiloes" element={<PrivateRoute><MeusLeiloes /></PrivateRoute>PrivateRoute>} />
+                                                        <Route path="/vendedor/:sellerId" element={<VendedorPerfil />} />
+                                                        <Route path="*" element={<Navigate to="/" replace />} />
+                                                </Route>Routes>
+                                        </Route>Router>
+                                  )
+                                  }
+                                
+                                export default App</Router>
